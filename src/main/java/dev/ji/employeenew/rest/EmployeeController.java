eb5b3e8ee0b199889c2f374c48d7f14687651455
yperunov <perunovyuri@gmail.com>
2022-01-31 22:58:05 +0300

    Imitial commit

--- src/main/java/dev/ji/employeenew/rest/EmployeeController.java
+++ src/main/java/dev/ji/employeenew/rest/EmployeeController.java
@@ -1,4 +1,47 @@
 package dev.ji.employeenew.rest;
 
+
+import dev.ji.employeenew.model.Employee;
+import dev.ji.employeenew.service.EmployeeService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employee")
 public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    public EmployeeController (EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/")
+    public String greetEmployee() {
+        return "Hello, employee";
+    }
+
+    @GetMapping("/add")
+    public String addEmployee(@RequestParam("firstName") String firstName,
+                              @RequestParam("lastName") String lastName) {
+        String addedEmployee = employeeService.addEmployee(firstName, lastName);
+        return addedEmployee;
+    }
+
+    @GetMapping("/remove")
+    public String removeEmployee(@RequestParam("firstName") String firstName,
+                                 @RequestParam("lastName") String lastName) {
+        String removedEmployee = employeeService.removeEmployee(firstName, lastName);
+        return removedEmployee;
+    }
+
+    @GetMapping("/find")
+    public Employee findEmployee(@RequestParam("firstName") String firstName,
+                              @RequestParam("lastName") String lastName) {
+        Employee foundEmployee = employeeService.findEmployee(firstName, lastName);
+        return foundEmployee;
+    }
+
 }
--- src/main/java/dev/ji/employeenew/service/EmployeeService.java
+++ src/main/java/dev/ji/employeenew/service/EmployeeService.java
@@ -1,4 +1,12 @@
 package dev.ji.employeenew.service;
 
+import dev.ji.employeenew.model.Employee;
+
 public interface EmployeeService {
+    String addEmployee(String firstName, String lastName);
+
+    String removeEmployee(String firstName, String lastName);
+
+    Employee findEmployee(String firstName, String lastName);
+
 }
